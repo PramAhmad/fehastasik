8e0b428411ccb03f96d78e4d96e820ef8ac15ce1
PramAhmad <pramuditaadit2810@gmail.com>
2024-04-07 15:52:05 +0700

    change icon

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="max-w-full px-4 sm:px-6 lg:px-8 py-12 container">
+    <h2 class="text-3xl font-semibold mb-6">Shop by Category</h2>
+    <div class="swiper-container" ref="swiper">
+      <div class="swiper-wrapper">
+        <div
+          v-for="category in categories"
+          :key="category.id"
+          class="swiper-slide"
+        >
+          <div class="bg-white shadow-md p-4 rounded-lg">
+            <img
+              :src="category.imageSrc"
+              :alt="category.imageAlt"
+              class="w-24 h-24 mx-auto mb-4"
+            />
+            <h3 class="text-lg font-semibold mb-2">{{ category.name }}</h3>
+            <p class="text-gray-700">{{ category.description }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+import "swiper/swiper-bundle.css";
+
+export default {
+  data() {
+    return {
+      categories: [
+        {
+          id: 1,
+          name: "Tas Wanita",
+          description: "Koleksi tas wanita terbaru",
+          imageSrc: "https://www.tailwind-kit.com/images/object/1.png",
+          imageAlt: "Tas Wanita",
+        },
+        {
+          id: 2,
+          name: "Tas Pria",
+          description: "Koleksi tas pria terbaru",
+          imageSrc: "https://www.tailwind-kit.com/images/object/2.png",
+          imageAlt: "Tas Pria",
+        },
+        {
+          id: 3,
+          name: "Tas Anak",
+          description: "Koleksi tas anak terbaru",
+          imageSrc: "https://www.tailwind-kit.com/images/object/3.png",
+          imageAlt: "Tas Anak",
+        },
+        {
+          id: 4,
+          name: "Tas Ransel",
+          description: "Koleksi tas ransel terbaru",
+          imageSrc: "https://www.tailwind-kit.com/images/object/4.png",
+          imageAlt: "Tas Ransel",
+        },
+        {
+          id: 5,
+          name: "Tas Selempang",
+          description: "Koleksi tas selempang terbaru",
+          imageSrc: "https://www.tailwind-kit.com/images/object/5.png",
+          imageAlt: "Tas Selempang",
+        },
+        {
+          id: 6,
+          name: "Tas Travel",
+          description: "Koleksi tas travel terbaru",
+          imageSrc: "https://www.tailwind-kit.com/images/object/6.png",
+          imageAlt: "Tas Travel",
+        },
+      ],
+    };
+  },
+  mounted() {
+    new Swiper(this.$refs.swiper, {
+      loop: true,
+      centeredSlides: true, // Tambahkan centeredSlides
+      loopedSlides: this.categories.length, // Set loopedSlides sesuai dengan jumlah slide yang di-loop
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  },
+};
+</script>
+
+<style scoped>
+/* Tambahkan gaya CSS khusus di sini jika diperlukan */
+.swiper-container {
+  width: 100%;
+  height: 100%;
+}
+.swiper-slide {
+  width: auto !important;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,56 @@
-<template lang="">
-    <div class="h-[200vh] container w-full relative">
-        <Navbar/>
-        <div class="bg-gray-100 flex relative z-20 items-center overflow-hidden">
-        <div class="container mx-auto px-6 flex relative py-20 min-h-screen items-center">
-            <div class="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
-                <span class="w-20 h-2 bg-gray-800 mb-12">
-                </span>
-                <h1 class="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none  text-gray-800">
-                   Has
-                    <span class="text-5xl sm:text-7xl">
-                        Tasik
-                    </span>
-                </h1>
-                <p class="text-sm sm:text-base text-gray-700 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta fugiat quaerat ratione error porro.s</p>
-                <div class="flex mt-8">
-                    <a href="#" class="uppercase py-2 px-4 rounded-lg bg-sky-500 border-2 border-transparent text-white text-md mr-4 hover:bg-sky-400">
-                        Lihat Product
-                    </a>
-                    <a href="#" class="uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-sky-500 text-sky-500  hover:bg-sky-500 hover:text-white text-md">
-                        Read more
-                    </a>
-                </div>
-            </div>
-            <div class=" sm:w-1/3 lg:w-3/5 relative">
-                <img src="https://www.tailwind-kit.com/images/object/10.png" class="max-w-xs md:max-w-sm m-auto"/>
-            </div>
+<template>
+  <div class="h-[200vh] container w-full relative">
+    <Navbar />
+    <div class="bg-gray-100 flex relative z-20 items-center overflow-hidden">
+      <div
+        class="container mx-auto px-6 flex relative py-20 min-h-screen items-center"
+      >
+        <div class="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
+          <span class="w-20 h-2 bg-gray-800 mb-12"></span>
+          <h1
+            class="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none text-gray-800"
+          >
+            Has
+            <span class="text-5xl sm:text-7xl"> Tasik </span>
+          </h1>
+          <p class="text-sm sm:text-base text-gray-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            fugiat quaerat ratione error porro.s
+          </p>
+          <div class="flex mt-8">
+            <a
+              href="#"
+              class="uppercase py-2 px-4 rounded-lg bg-sky-500 border-2 border-transparent text-white text-md mr-4 hover:bg-sky-400"
+            >
+              Lihat product
+            </a>
+            <a
+              href="#"
+              class="hidden md:block uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-sky-500 text-sky-500 hover:bg-sky-500 hover:text-white text-md"
+            >
+              Read more
+            </a>
+          </div>
         </div>
+        <div class="sm:w-1/3 lg:w-3/5 relative">
+          <img
+            src="https://lh3.googleusercontent.com/pw/AP1GczOBG4wEYzXBuOJKCFAtCTPOHY7H6UtGa3-JNjAYTGQai3scvpj5GzFgGmDfb6BOgezMIk9Zbr6UKz4whiyKiABdN4P75x_uTareEFS13ReaaTWggAERLjfDWvUx9wWixiubN8FIB4b7t5qkFdSRDrjpaw=w621-h932-s-no-gm?authuser=0"
+            class="max-w-xs md:max-w-xl m-auto"
+          />
+        </div>
+      </div>
     </div>
-    </div>
-</template>
-<script setup>
-import Navbar from "../components/Navbar.vue";
 
-components: {
-  Navbar;
-}
+    <!-- category -->
+    <!-- <CategoryCard /> -->
+  </div>
+</template>
+  
+  <script setup>
+import Navbar from "../components/Navbar.vue";
+import CategoryCard from "../components/CategoryCard.vue";
+const categories = [];
 </script>
-<style>
+  
+  <style scoped>
 </style>
