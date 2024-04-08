4d3ad3f05c6ef6309d171498db5e8fbf8932cd59
PramAhmad <pramuditaadit2810@gmail.com>
2024-04-08 19:48:02 +0700

    add card product

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="flex relative z-20 items-center overflow-hidden">
         <div
-          class="container mx-auto px-6 flex relative pt-16 min-h-screen items-center"
+          class="container mx-auto px-6 flex relative pt-10 min-h-screen items-center"
         >
           <div class="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
             <span class="w-20 h-2 bg-gray-800 mb-12"></span>
@@ -43,7 +43,7 @@
       </div>
 
       <!-- all -->
-      <section class="py-16">
+      <section class="pt-10 pb-16">
         <div class="py-4 px-2 mx-auto max-w-screen-xl sm:py-4 lg:px-6">
           <div
             class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 h-full"
@@ -56,7 +56,7 @@
                 class="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow"
               >
                 <img
-                  src="https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="https://lh3.googleusercontent.com/pw/AP1GczN5ayhgzYNE9MpepJ0voqO48I1jnZxmgn-jzjAKTVYFxm-E8223_CYueL1F3b9JjEtAhhYvoykA-NHtsbRDf_iseRXRhYU9YPfikL0Sbm7-QQRedcuAw0-ivNAQRdXubxcXzkc7y_u1gXS1Qz5yPz8R=w1657-h932-s-no-gm?authuser=0"
                   alt=""
                   class="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                 />
@@ -76,7 +76,7 @@
                 class="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 mb-4"
               >
                 <img
-                  src="https://images.unsplash.com/photo-1504675099198-7023dd85f5a3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="https://lh3.googleusercontent.com/pw/AP1GczOYn3Buq4YOHc1XZraJab5mZ10KgoPDIUnZqAR9muhFGWBxe5ZwIvhdRIfABFbMc8ZfJAMcexUemtYfalydRNUlxna4HKGbCAv6XtqnbLyczQ5OiC-ASCP9izjYW1t5GKosd7omDMrOxbb6KlOXhmq4=w564-h844-s-no-gm?authuser=0"
                   alt=""
                   class="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                 />
@@ -95,7 +95,7 @@
                   class="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
                 >
                   <img
-                    src="https://images.unsplash.com/photo-1571104508999-893933ded431?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    src="https://lh3.googleusercontent.com/pw/AP1GczPrJafp2KH7-KnxMD2b5rZB63lRJJGG7e8hekZ57l38iAPfB69KvyEOrKoO-IiX6rfV2tx12L29vvQ4cnr8oj_Y0UxjVCAizniq9SS2ITi4eLEV8JGay1V7SSkhIZsBkEoAVF7HwnuwbpJcLU8njk1X=w525-h932-s-no-gm?authuser=0"
                     alt=""
                     class="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                   />
@@ -113,7 +113,7 @@
                   class="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
                 >
                   <img
-                    src="https://images.unsplash.com/photo-1626897505254-e0f811aa9bf7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    src="https://lh3.googleusercontent.com/pw/AP1GczN4KGglNjalD7dJEJoERrSCAQDZmaog-__6LzuZ6kOQwjBZAhE5alAbKjv9SVwwozVe3V5aVGG9_x5AJU1SBELE57TDlTYvYd3It3eIgQtlVDRCUoOgBWfk95FhrRJKBTU331X4mAzwo1B20elR27gJ=w564-h421-s-no-gm?authuser=0"
                     alt=""
                     class="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                   />
@@ -136,7 +136,7 @@
                 class="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow"
               >
                 <img
-                  src="https://images.unsplash.com/photo-1693680501357-a342180f1946?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="https://lh3.googleusercontent.com/pw/AP1GczPG-gY9XtFOJRXWAPUeE5EIwF4g6KpDKBXbDUQsYP6ocNRDyxL0sJz0Y_xPbOPlEj6EBP9_2G1GwUqrOPE7NMCZEfq3-2dG6NyhmbAzSoE_MFA4L15zS2fnMoxxBaajARiUpy2yWv6DMKw8xIwQb_05=w564-h376-s-no-gm?authuser=0   "
                   alt=""
                   class="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                 />
@@ -156,6 +156,192 @@
 
       <!-- category -->
       <CategoryCard />
+
+      <!-- New Product -->
+      <section class="py-10">
+        <div class="container flex px-6">
+          <div
+            class="group my-10 flex w-full max-w-xs flex-col overflow-hidden bg-white rounded-md border border-gray-200"
+          >
+            <a class="relative flex h-80 w-80 overflow-hidden" href="#">
+              <img
+                class="absolute top-0 right-0 h-full w-full object-cover"
+                src="https://images.tokopedia.net/img/cache/700/VqbcmM/2021/9/13/809c9952-b47b-4099-8f61-d2dac63d4e11.jpg"
+                alt="product image"
+              />
+
+              <div
+                class="absolute -right-16 bottom-0 mr-2 mb-4 space-y-2 transition-all duration-300 group-hover:right-0"
+              >
+                <button
+                  class="flex h-10 w-10 items-center justify-center bg-gray-900 text-white transition hover:bg-gray-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </button>
+                <button
+                  class="flex h-10 w-10 items-center justify-center bg-gray-900 text-white transition hover:bg-gray-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="mr-2 h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </a>
+            <div class="mt-4 pb-5">
+              <a href="#">
+                <h5 class="text-center tracking-tight text-gray-500">
+                  Payung Pramudita
+                </h5>
+              </a>
+              <div class="mb-5 flex justify-center">
+                <p>
+                  <span class="text-sm font-bold text-gray-900"
+                    >Rp 30.000
+                  </span>
+                  <span class="text-sm text-gray-400 line-through"
+                    >Rp 50.000</span
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md"
+          >
+            <a
+              class="relative mx-3 mt-3 flex h-60 w-fulloverflow-hidden rounded-xl"
+              href="#"
+            >
+              <img
+                class="object-cover"
+                src="https://images.tokopedia.net/img/cache/700/VqbcmM/2021/9/13/809c9952-b47b-4099-8f61-d2dac63d4e11.jpg"
+                alt="product image"
+              />
+              <span
+                class="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white"
+                >20% OFF</span
+              >
+            </a>
+            <div class="mt-4 px-5 pb-5">
+              <a href="#">
+                <h5 class="text-xl tracking-tight text-slate-900">
+                  Payung Dita
+                </h5>
+              </a>
+              <div class="mt-2 mb-5 flex items-center justify-between">
+                <p>
+                  <span class="text-3xl font-bold text-slate-900"
+                    >Rp 50.000</span
+                  >
+                  <span class="text-sm text-slate-900 line-through"
+                    >Rp 40.000</span
+                  >
+                </p>
+              </div>
+              <div class="flex items-center mb-5">
+                <svg
+                  aria-hidden="true"
+                  class="h-5 w-5 text-yellow-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  ></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  class="h-5 w-5 text-yellow-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  ></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  class="h-5 w-5 text-yellow-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  ></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  class="h-5 w-5 text-yellow-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  ></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  class="h-5 w-5 text-yellow-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  ></path>
+                </svg>
+                <span
+                  class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold"
+                  >5.0</span
+                >
+              </div>
+              <a
+                href="#"
+                class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="mr-2 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                Add to cart</a
+              >
+            </div>
+          </div>
+          <!-- component -->
+        </div>
+      </section>
     </div>
   </div>
 </template>
